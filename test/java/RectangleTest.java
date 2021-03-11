7de02213223b6ab483c531c6cfeaaa9e5e59bb0c
harinimanu <harinimanu123@gmail.com>
2021-03-11 10:49:21 +0530

    test if area is negative if one side is negative for rectangle

--- test/java/RectangleTest.java
+++ test/java/RectangleTest.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.css.Rect;
 import utility.Rectangle;
 
 public class RectangleTest {
@@ -10,8 +11,19 @@ public class RectangleTest {
         int length = 5;
         int breadth = 10;
         Rectangle newRectangle = new Rectangle(length,breadth);
-
         int expectedArea = 50;
+        
+        assertEquals(expectedArea,newRectangle.area());
+    }
+
+    @Test
+
+    public void TestAreaIsNegativeForOneNegativeValuedSides(){
+        int length = -10;
+        int breadth = 4;
+        Rectangle newRectangle = new Rectangle(length,breadth);
+        int expectedArea = -40;
+
         assertEquals(expectedArea,newRectangle.area());
     }
 }
